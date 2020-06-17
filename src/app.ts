885c4161ed6e37f16d93e5ab11d93ce59da7de5f
Mykola Shydlovskyi <mshydlovskyi@n-ix.com>
2020-06-17 10:46:08 +0300

    Create basic API structure - Add mongoose connection - Add mountRouter - Add cors configuraton - Add .env.example

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import { NextFunction, Request, Response } from 'express';
-
-/*import * as cors from 'cors';*/  //TODO
+import * as cors from 'cors';
 import * as express from 'express';
+import { NextFunction, Request, Response } from 'express';
 import * as rateLimit from 'express-rate-limit';
 import * as helmet from 'helmet';
 import * as dotenv from 'dotenv';
 import * as morgan from 'morgan';
-/*import * as mongoose from 'mongoose';*/ //TODO
+import * as mongoose from 'mongoose';
 import * as path from 'path';
 
 import { config } from './config';
+import { apiRouter, notFoundRouter } from './routes';
 
 dotenv.config();
 
@@ -27,14 +27,27 @@ class App {
       this.app.use(morgan('dev'));
       this.app.use(helmet());
       this.app.use(serverRequestLimit);
+      this.app.use(cors({
+        origin: this.corsConfiguration
+      }));
 
       this.app.use(express.json());
       this.app.use(express.urlencoded({extended: true}));
 
       this.app.use(express.static(path.resolve((global as any).appRoot, 'public')));
 
+      this.mountRoutes();
+      this.setupDB();
+
       this.app.use(this.customErrorHandler);
 
+    }
+
+    private setupDB(): void {
+      mongoose.connect(config.MONGODB_URL, {useNewUrlParser: true});
+
+      const db = mongoose.connection;
+      db.on('error', console.log.bind(console, 'MONGO ERROR'));
     }
 
     private customErrorHandler(err: any, req: Request, res: Response, next: NextFunction): void {
@@ -44,6 +57,25 @@ class App {
           message: err.message || 'Unknown Error',
           code: err.code
         });
+    }
+
+    private corsConfiguration = (origin: any, callback: any) => {
+      const whiteList = config.ALLOWED_ORIGIN.split(';');
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (!whiteList.includes(origin)) {
+        return callback(new Error('Cors not allowed'), false);
+      }
+
+      return callback(null, true);
+    }
+
+    private mountRoutes(): void {
+      this.app.use('/api', apiRouter);
+      this.app.use('*', notFoundRouter);
     }
 }
 
