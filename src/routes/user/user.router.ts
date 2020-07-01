6ea7a233ee96a5501db9709aaa4c1ee50e0bd5c9
Mykola Shydlovskyi <mshydlovskyi@n-ix.com>
2020-07-01 14:03:33 +0300

    - add expressOpenApi validator - create temporary solution for controller

--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 
 import UserController from '../../controllers/user/user.controller';
 import UserService from '../../services';
@@ -9,7 +9,8 @@ const userController = new UserController(userService);
 
 const router = Router();
 
-router.post('/', checkIsUserValidMiddleware, userController.createUser);
+router.post('/', checkIsUserValidMiddleware,
+    (req: Request, res: Response, next: NextFunction) => userController.createUser(req, res, next) );
 
 router.get('/:userId', userController.getUserById.bind(userController));
 
